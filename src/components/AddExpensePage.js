7f9665f0c7dc86bc7df846a1e8804428cba5b32d
David Rubio Vidal <davidrv87@gmail.com>
2018-02-03 19:50:51 +1300

    Add Create Expense form

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import ExpenseForm from './ExpenseForm';
+
 const AddExpensePage = () => (
     <div>
-        This is the add expense page
+        <h1>Create Expense</h1>
+        <ExpenseForm />
     </div>
 );
 
---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { addExpense } from "../actions/expenses";
+
+class ExpenseForm extends React.Component {
+    state = {
+        description: '',
+        amount: '',
+        note: ''
+    }
+    onDescriptionChange = (e) => {
+        const description = e.target.value;
+        this.setState(() => ({ description }));
+    };
+    onNoteChange = (e) => {
+        const note = e.target.value;
+        this.setState(() => ({ note }));
+    };
+    onAmountChange = (e) => {
+        const amount = e.target.value;
+        if (amount.match(/^\d+(\.\d{0,2})?$/)) {
+            this.setState(() => ({ amount }));
+        }
+    };
+    onFormSubmit = (e) => {
+        e.preventDefault();
+
+        this.props.dispatch(addExpense(this.state));
+    };
+    render() {
+        return (
+            <div>
+                <form
+                    onSubmit={this.onFormSubmit}
+                >
+                    <input
+                        type="text"
+                        placeholder="Description"
+                        autoFocus
+                        value={this.state.description}
+                        onChange={this.onDescriptionChange}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Amount"
+                        value={this.state.amount}
+                        onChange={this.onAmountChange}
+                    />
+                    <textarea
+                        placeholder="Add a note for your expense (optional)"
+                        value={this.state.note}
+                        onChange={this.onNoteChange}
+                    />
+                    <button>Create Expense</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default connect()(ExpenseForm);
