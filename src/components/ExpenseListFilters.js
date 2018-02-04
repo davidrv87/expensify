74fb5e53471137333ac459f57df2c84503c435de
David Rubio Vidal <davidrv87@gmail.com>
2018-02-04 20:57:22 +1300

    Add filter by date using DateRangePicker

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,30 +1,59 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import 'react-dates/initialize'; // Required as per v13 - see https://github.com/airbnb/react-dates/issues/797
+import { DateRangePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
 
-import { setTextFilter, sortByAmount, sortByDate } from '../actions/filters';
+import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from '../actions/filters';
 
-const ExpenseListFilters = props => (
-    <div>
-        <input
-            type="text"
-            value={props.filters.text}
-            onChange={(e) => {
-                // When we connect a Component, we have access to dispatch() via props.dispatch
-                // This can be seen using the React Dev Tool in Chrome
-                props.dispatch(setTextFilter(e.target.value));
-            }}
-        />
-        <select
-            value={props.filters.sortBy}
-            onChange={(e) => {
-                e.target.value === 'date' ? props.dispatch(sortByDate()) : props.dispatch(sortByAmount());
-            }}
-        >
-            <option value="date">Date</option>
-            <option value="amount">Amount</option>
-        </select>
-    </div>
-);
+class ExpenseListFilters extends React.Component {
+    state = {
+        calendarFocused: null
+    };
+    onDatesChange = ({ startDate, endDate }) => {
+        this.props.dispatch(setStartDate(startDate));
+        this.props.dispatch(setEndDate(endDate));
+    };
+    onFocusChange = calendarFocused => {
+        this.setState(() => ({ calendarFocused }));
+    };
+    render() {
+        return (
+            <div>
+                <input
+                    type="text"
+                    value={this.props.filters.text}
+                    onChange={(e) => {
+                        // When we connect a Component, we have access to dispatch() via props.dispatch
+                        // This can be seen using the React Dev Tool in Chrome
+                        this.props.dispatch(setTextFilter(e.target.value));
+                    }}
+                />
+                <select
+                    value={this.props.filters.sortBy}
+                    onChange={(e) => {
+                        e.target.value === 'date' ? this.props.dispatch(sortByDate()) : this.props.dispatch(sortByAmount());
+                    }}
+                >
+                    <option value="date">Date</option>
+                    <option value="amount">Amount</option>
+                </select>
+                <DateRangePicker
+                    startDate={this.props.filters.startDate}
+                    startDateId='startDate'
+                    endDate={this.props.filters.endDate}
+                    endDateId='endDate'
+                    onDatesChange={this.onDatesChange}
+                    focusedInput={this.state.calendarFocused}
+                    onFocusChange={this.onFocusChange}
+                    showClearDates={true}
+                    numberOfMonths={1}
+                    isOutsideRange={() => false}
+                />
+            </div>
+        );
+    }
+}
 
 const mapStateToProps = state => ({
     filters: state.filters
